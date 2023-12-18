93c73353d99475463e810229def7341956d1471c
louloulouloulouloulou <lourencopinto6@gmail.com>
2023-12-18 15:27:00 +0000

    s
    s

--- grid 8x8 3/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/grid 8x8 3.AssemblyInfo.cs
+++ grid 8x8 3/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/grid 8x8 3.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "f634dc76-8be4-4f53-86ff-beb5601b115e")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "grid 8x8 3")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("grid 8x8 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("grid 8x8 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grid 8x8 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
